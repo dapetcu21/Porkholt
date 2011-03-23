36be12684a61ac3b2a271962bfefdcdb2c0df6ad
Marius Petcu <dapetcu21@gmail.com>
2011-03-23 14:22:56 +0000

    RGB texture loading fixed

--- Core/PHImage.cpp
+++ Core/PHImage.cpp
@@ -135,7 +135,7 @@ void PHImage::loadToTexture(PHObject * sender, void * ud)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, actWidth, actHeight, 0, 
+	glTexImage2D(GL_TEXTURE_2D, 0, format, actWidth, actHeight, 0, 
 				 format, GL_UNSIGNED_BYTE, (uint8_t *)ud);	
 }
 
