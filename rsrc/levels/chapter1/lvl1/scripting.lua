e2c68be2d8c75daf20fc9f92f1f4fb724c4c04fa
Marius Petcu <dapetcu21@gmail.com>
2011-07-24 20:04:46 +0300

    Some housecleaning, you can now read it from the beginning to the end

--- rsrc/levels/chapter1/lvl1/scripting.lua
+++ rsrc/levels/chapter1/lvl1/scripting.lua
@@ -5,18 +5,8 @@ player.initialPosition = player:position();
 mayor.initialPosition = mayor:position();
 camera.initialPosition = camera:position();
 
-
-function sign:questTapped()
-	sign:setDialog("Tilt your device\nto move around");
-end
-
-function afterDialogs()
-	player:setBraked(false);
-	player:setUserInput(true);
-	sign:setDialog("Tilt your device\nto move around",sign.setShowsQuest,sign,true)
-end
-
-function  addDialogs()
+local timer = PHTimer:new(nil,1,false);
+timer:setCallback(function()
 	mayor:addDialog("Benno, I know that you are a good guy and I know that your actions are noble in origin,");
 	mayor:addDialog("but the results are destroying this city and I, as the mayor of Beansville,");
 	mayor:addDialog("feel responsible to firstly assure the safety of the citizens.");
@@ -25,31 +15,51 @@ function  addDialogs()
 	mayor:addDialog("Every year, the pipes that transport the Goo build up a lot of pressure")
 	mayor:addDialog("Your job is simple: Release the pressure valve.");
 	mayor:addDialog("AND DON'T TOUCH ANYTHING ELSE!");
-	player:addDialog("Yeah! That sounds simple enough!",afterDialogs);
+	player:addDialog("Yeah! That sounds simple enough!",function()
+	player:setBraked(false);
+	player:setUserInput(true);
+	sign:setDialog("Tilt your device\nto move around",sign.setShowsQuest,sign,true);
+	end);
+end
+timer:schedule();
+
+function sign:questTapped()
+	sign:setDialog("Tilt your device\nto move around");
 end
 
-function faded1()
+function sensor1:objectEntered()
+	PHWorld:fadeToColor(colorWithRGBA(0,0,0,1),function()
 	player:setVelocity(vector(0,0));
 	player:setAngularVelocity(0);
 	player:setPosition(marker1:position());
 	camera:setPosition(marker3:position());
 	camera:setFollowsPlayer(false);
 	PHWorld:dismissFading();
+	end);
 end
 
-function endalldialogs()
-	player:setBraked(false);
-	player:setUserInput(true);
-end
-
-function faded3ended()
-	mayor:addDialog("That's it! You've outdone yourself!");
-	mayor:addDialog("You'll leave this city right now and never return again!");
-	player:addDialog("Wait! Wait!\nI'm sorry!\nBut I can...");
-	mayor:addDialog("GET OUT!",endalldialogs);
-end
-
-function _faded3()
+function sensor2:objectEntered()
+	player:setBraked(true);
+	player:setUserInput(false);
+	PHWorld:fadeToColor(colorWithRGBA(0.9,0.1,0.1,1),function()
+	player:viewWithTag(20):setImage("ball");
+	player:setVelocity(vector(0,0));
+	player:setAngularVelocity(0);
+	player:setFlipped(false);
+	PHWorld:dismissFading();
+	mayor:setPosition(marker2:position());
+	piperoomdoor:destroy();
+	mayor:setDynamic(true);
+	--the mayor walks towards you
+	mayor:walkTo(marker1:position(),2,function()
+	mayor:setDynamic(false);
+	player:setFlipped(false);
+	mayor:addDialog("Beeno, what have you done!");
+	mayor:addDialog("You're red and the whole town is out of power!");
+	player:addDialog("Yeah.. Well... ummm...");
+	player:addDialog("I kinda screwed up and it all blew up",function()
+	PHWorld:fadeToColor(colorWithRGBA(0,0,0,1),function() 
+	--after the dialog we fade to black and go back to the city
 	player:setVelocity(vector(0,0));
 	player:setAngularVelocity(0);
 	player:setPosition(player.initialPosition);
@@ -59,50 +69,28 @@ function _faded3()
 	player:setFlipped(true);
 	mayor:setFlipped(false);
 	sign:destroy();
-	PHWorld:dismissFading(faded3ended);
 	
+	--destroy the sensor and put a godray instead
 	local obj = objectWithClass("PHLLevelEnd");
 	obj.pos = sensor1:position();
 	objectAddBox(obj,-0.25,-0.25,0.5,0.5);
 	PHWorld:insertObject(obj);
 	sensor1:destroy();
+	
+	--final dialog
+	PHWorld:dismissFading(function()
+	mayor:addDialog("That's it! You've outdone yourself!");
+	mayor:addDialog("You'll leave this city right now and never return again!");
+	player:addDialog("Wait! Wait!\nI'm sorry!\nBut I can...");
+	mayor:addDialog("GET OUT!",function()
+	player:setBraked(false);
+	player:setUserInput(true);
+	end);
+	
+	end);
+	
+	end);
+	end);
+	end);
+	end);
 end
-
-function faded3()
-	PHWorld:fadeToColor(colorWithRGBA(0,0,0,1),_faded3);
-end
-
-function walked()
-	mayor:setDynamic(false);
-	player:setFlipped(false);
-	mayor:addDialog("Beeno, what have you done!");
-	mayor:addDialog("You're red and the whole town is out of power!");
-	player:addDialog("Yeah.. Well... ummm...");
-	player:addDialog("I kinda screwed up and it all blew up",faded3);
-end
-
-function faded2()
-	player:viewWithTag(20):setImage("ball");
-	player:setVelocity(vector(0,0));
-	player:setAngularVelocity(0);
-	player:setFlipped(false);
-	PHWorld:dismissFading();
-	mayor:setPosition(marker2:position());
-	piperoomdoor:destroy();
-	mayor:setDynamic(true);
-	mayor:walkTo(marker1:position(),2,walked);
-end
-
-function sensor2:objectEntered()
-	player:setBraked(true);
-	player:setUserInput(false);
-	PHWorld:fadeToColor(colorWithRGBA(0.9,0.1,0.1,1),faded2);
-end
-
-function sensor1:objectEntered()
-	PHWorld:fadeToColor(colorWithRGBA(0,0,0,1),faded1);
-end
-
-local timer = PHTimer:new(nil,1,false);
-timer:setCallback(addDialogs);
-timer:schedule();
