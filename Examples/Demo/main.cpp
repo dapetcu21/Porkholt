ca41b7ee9e3819143683c3785f7bf0f7ab8f2ba1
Marius Petcu <dapetcu21@gmail.com>
2012-04-26 07:24:59 +0300

    Porkholt labs logo

--- Examples/Demo/main.cpp
+++ Examples/Demo/main.cpp
@@ -7,11 +7,104 @@
 //
 
 #import "PHStartGame.h"
-#include "PHTestViewController.h"
+#include "PHViewController.h"
+#include "PHImage.h"
+#include "PHImageView.h"
+
+class PHSlide : public PHViewController
+{
+protected:
+    virtual void screenTapped(int count) {
+        
+    }
+    
+    int cnt;
+    
+    void _screenTapped(PHObject * sender, void * ud)
+    {
+        screenTapped(cnt++);
+    }
+    
+public:
+    
+    PHSlide() : cnt(0) {
+        PHMessage::messageWithName("thingTapped")->addListener(this, (PHCallback)&PHSlide::_screenTapped);
+    }
+    
+    ~PHSlide() {
+        PHMessage::messageWithName("thingTapped")->removeListener(this);
+    }
+};
+
+class PHTitle : public PHSlide
+{
+protected:
+    PHView * loadView(const PHRect & r)
+    {
+        PHView * v = new PHView(r);
+        v->setAutoresizeMask(PHView::ResizeAll);
+        v->setAutoresizesSubviews(true);
+        return v;
+    }
+    
+    void screenTapped(int count)
+    {
+        if (count == 0)
+        {
+            PHView * v = getView();
+            PHImage * img = gm->imageNamed("porkholt_labs");
+            PHRect rr(0, 0, img->width(), img->height());
+            PHRect r = v->bounds();
+            rr.x = (r.width - rr.width)/2;
+            rr.y = (r.height - rr.height)/2;
+            PHImageView * vv = new PHImageView(rr);
+            vv->setImage(img);
+            vv->setAutoresizeMask(PHView::ResizeFlexibleBorders);
+            vv->setScaleX(5.0f);
+            vv->setScaleY(5.0f);
+            vv->setTintColor(PHClearColor);
+            vv->beginCinematicAnimation(1.0f, PHCinematicAnimator::FadeOutFunction);
+            vv->animateScale(PHSize(1/5.0f, 1/5.0f));
+            vv->animateCustomColor(PHWhiteColor);
+            vv->commitCinematicAnimation();
+            v->addChild(vv);
+            vv->release();
+        }
+    }
+};
+
+
+
+class PHCaptureView : public PHView
+{
+public:
+    PHCaptureView() {};
+    PHCaptureView(const PHRect & r) : PHView(r) {}
+    
+protected:
+    void touchEvent(PHEvent * evt)
+    {
+        if (((evt->type() == PHEvent::touchDown) || (evt->type() == PHEvent::touchUp)) && PHPointInRect(toMyCoordinates(evt->location()), this->bounds()))
+        {
+            evt->setHandled(true);
+            if (evt->type() == PHEvent::touchUp)
+                PHMessage::messageWithName("thingTapped")->broadcast(this, NULL);
+        }
+    }
+};
 
 void PHGameEntryPoint(PHGameManager * gm)
-{    
-    PHTestViewController * vc = new PHTestViewController();
+{   
+    PHCaptureView * v = new PHCaptureView(gm->navigationController()->getView()->frame());
+    gm->navigationController()->getView()->setAutoresizeMask(PHView::ResizeAll);
+    gm->navigationController()->getView()->setAutoresizesSubviews(true);
+    v->setAutoresizesSubviews(true);
+    v->setUserInput(true);
+    v->addChild(gm->navigationController()->getView());
+    gm->setMainView(v);
+    v->release();
+    
+    PHViewController * vc = new PHTitle();
 	vc->init(gm);
     gm->navigationController()->pushViewController(vc);
 }
