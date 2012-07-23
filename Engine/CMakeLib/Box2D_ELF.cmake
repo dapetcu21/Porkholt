a9d4f34a094bd6cf446adb8bcf6ac4556fa3ea09
Marius Petcu <dapetcu21@gmail.com>
2012-07-23 22:38:41 +0300

    Forgot stuff

--- Engine/CMakeLib/Box2D_ELF.cmake
+++
@@ -1,3 +0,0 @@
-include(${PH_ENGINE_PATH}/CMakeLib/Box2D_Common.cmake)
-
-add_library(Box2D_X11 ${PH_B2D_SRCS} ${PH_B2D_HEADERS})
--- Engine/CMakeLib/Box2D_iOS.cmake
+++
@@ -1,7 +0,0 @@
-include(${PH_ENGINE_PATH}/CMakeLib/Box2D_Common.cmake)
-
-add_library(Box2D_iOS ${PH_B2D_SRCS} ${PH_B2D_HEADERS})
-set_target_properties(Box2D_iOS PROPERTIES XCODE_ATTRIBUTE_GCC_VERSION "com.apple.compilers.llvmgcc42")
-set_target_properties(Box2D_iOS PROPERTIES XCODE_ATTRIBUTE_GCC_THUMB_SUPPORT "NO")
-set_target_properties(Box2D_iOS PROPERTIES XCODE_ATTRIBUTE_GCC_SYMBOLS_PRIVATE_EXTERN "YES")
-set_target_properties(Box2D_iOS PROPERTIES XCODE_ATTRIBUTE_GCC_C_LANGUAGE_STANDARD "c99")
